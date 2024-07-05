4b2c555abfeb017baba332050ccfcbc62ed5903f
dkultyshev <dkultyshev@neoflex.ru>
2024-07-05 17:40:20 +0300

    update config.py and database.py

--- PETapp/migrations/env.py
+++ PETapp/migrations/env.py
@@ -9,14 +9,15 @@ from alembic import context
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
-from PETapp.database import Base, DATABASE_URL
+from PETapp.database import settings
+from PETapp.database import Base
 from PETapp.hotels.models import Hotels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.

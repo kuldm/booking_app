924cc343d079a5271f6df6d6d9d2284036b96d81
dkultyshev <dkultyshev@neoflex.ru>
2024-07-08 03:22:07 +0300

    Added user logining functionality

--- PETapp/users/auth.py
+++ PETapp/users/auth.py
@@ -1,4 +1,13 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
 from passlib.context import CryptContext
+from pydantic import EmailStr
+
+from PETapp.users.service import UserService
+
+from secrets import token_bytes
+from base64 import b64encode
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +18,20 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, "1qaFgdDFDSF", "HS256"
+    )
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UserService.find_one_or_none(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
--- PETapp/users/router.py
+++ PETapp/users/router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status, Response
 
-from PETapp.users.shemas import SUserRegister
+from PETapp.users.shemas import SUserAuth
 
 from PETapp.users.service import UserService
-from PETapp.users.auth import get_password_hash
+from PETapp.users.auth import get_password_hash, verify_password, authenticate_user, create_access_token
 
 router = APIRouter(
     prefix="/auth",
@@ -12,9 +12,19 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UserService.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UserService.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
